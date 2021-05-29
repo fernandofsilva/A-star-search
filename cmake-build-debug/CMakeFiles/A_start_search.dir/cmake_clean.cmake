4f9b1f9adae30a2f468e68e09fbae93f13a00951
Fernando Ferreira <fernandosk8@gmail.com>
2021-05-29 09:20:14 -0300

    First Revision

---
+++ cmake-build-debug/CMakeFiles/A_start_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "A_start_search"
+  "A_start_search.pdb"
+  "CMakeFiles/A_start_search.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A_start_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
